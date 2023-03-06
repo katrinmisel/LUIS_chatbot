f313e18bd5d969c3f5fcdac1ee84d66e01285a2c
Katrin <katrinmisel@gmail.com>
2023-03-06 21:31:39 +0100

    add env secrets

---
+++ bot/config.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+"""Configuration for the bot."""
+
+import os
+
+
+class DefaultConfig:
+    """Configuration for the bot."""
+
+    PORT = 8000
+    APP_ID = os.environ.get('APP_ID')
+    APP_PASSWORD = os.environ.get('APP_PASSWORD')
+    LUIS_APP_ID = os.environ.get('LUIS_APP_ID')
+    LUIS_API_KEY = os.environ.get('LUIS_API_KEY')
+    LUIS_API_HOST_NAME = os.environ.get('LUIS_API_HOST_NAME')
+    APPINSIGHTS_INSTRUMENTATION_KEY = os.environ.get(
+        'APPINSIGHTS_INSTRUMENTATION_KEY'
+    )
